dc738c65de150fe8ad2123493908d208113641cf
wookim456@gmail.com <wookim789@gmail.com>
2020-11-05 15:17:25 +0900

    코드워크 프론트 작업 시작 by react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+/* eslint-disable */
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // ES6 destructurning 문법
+  let posts = ["sclae", "chord", "diatonic"];
+  let [subject, funcSubject] = useState(posts);
+  
+  let days = ["2020.11.01", "2020.11.02", "2020.11.03"];
+  let [day, funcDay] = useState(days);
+
+  let [likeCnt, funcLikeCnt] = useState(0);
+
+  function changeSubject(){
+      let copySubject = [...subject]
+      if(copySubject[0] == 'sclae'){
+          copySubject[0] = 'Scale'
+      }else{
+        copySubject[0] = 'sclae'
+      }
+      funcSubject(copySubject)
+  }
   return (
+    // jsx 문법 (html 대신) : class -> className
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="black-nav">
+            <div>Chord walk</div>
+        </div>
+        <button onClick = { ()=>{ changeSubject() } } >버튼</button>
+        <div className="list">
+            <h3> { subject[0] } <span onClick = { ()=> { funcLikeCnt(likeCnt + 1) } } >👍</span> {likeCnt} </h3>
+            <p> { day[0] } </p>
+            <hr/>
+            <h3> { subject[1] } </h3>
+            <p> { day[1] } </p>
+            <hr/>
+            <h3> { subject[2] } </h3>
+            <p> { day[2] } </p>
+            <hr/>
+        </div>
     </div>
   );
 }
-
 export default App;
