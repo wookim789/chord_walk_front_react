7d30ea3e7c1b2b923190ecd07a7338a92b3a6116
wookim789 <wookim789@gmail.com>
2020-11-07 18:07:52 +0900

    키보드 입력 이벤트 처리헤야함

--- src/component/piano/keys/Key.js
+++ src/component/piano/keys/Key.js
@@ -4,10 +4,13 @@ function Key(props){
   const wavMap = {65 : '040.wav', 87 : '041.wav', 83 : '042.wav', 69 : '043.wav', 68 : '044.wav', 70 : '045.wav', 84 : '046.wav', 71 : '047.wav', 89 : '048.wav', 72 : '049.wav', 85 : '050.wav', 74 : '051.wav', 75 : '052.wav', 79 : '053.wav', 76 : '054.wav', 80 : '055.wav', 186 : '056.wav'}
   const setWavFile = (key) => {
     return "http://carolinegabriel.com/demo/js-keyboard/sounds/" + wavMap[key];
-  }
+  };
+  const soundPlay = (e) =>{
+    console.log(e.key);
+  };
   return(
     <div data-key={ props.dataNumKey } className={ props.keyKind} data-note= { props.noteName }>
-      <span className="hints"> { props.hintKey } </span>
+      <span  className="hints"> { props.hintKey } </span>
       <audio data-key={ props.dataNumKey } src={ setWavFile(props.dataNumKey) }></audio>
     </div>
   )
